9599e631622ea9575263b9e7210659a79cffd719
MohamedNaser92 <mohamed.naser.m92@gmail.com>
2023-10-25 23:17:09 +0300

    Merge 'employee' into 'task'

---
+++ src/module/task/task.controller.ts
@@ -0,0 +1,9 @@
+import { NextFunction, RequestHandler } from 'express';
+
+export const createTask: RequestHandler = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const scrumId = ;
+};
