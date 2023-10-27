70cff21c956e56879c09133905b4d12916631dd5
MohamedNaser92 <mohamed.naser.m92@gmail.com>
2023-10-27 20:39:47 +0200

    update task and getallemp for scrum

--- src/module/employee/employee.validation.ts
+++ src/module/employee/employee.validation.ts
@@ -2,6 +2,7 @@ import Joi from 'joi';
 import { IEmployee } from '../../types/employee.types';
 import { CUSTOM_FIELDS_SCHEMAS } from '../../constants/schema_validation_fields';
 import { AdminSchemaType } from '../../../DB/model/admin.model';
+import { UserRole } from '../../constants/user.role';
 
 interface ICreateEmployeeSchema extends IEmployee {}
 
@@ -10,16 +11,23 @@ interface IChangeEmployeePassword extends Pick<IEmployee, 'password'> {
 	newPassword: string;
 	employeeId: string;
 }
+interface IOnlyObjectId {
+	scrumId: string;
+}
 
 export const createEmployeeSchema = Joi.object<ICreateEmployeeSchema>({
 	employeeName: Joi.string().min(3).max(20).required(),
 	email: Joi.string().email().required(),
 	password: Joi.string().required(),
-	role: Joi.string().valid('scrumMaster', 'member').required(),
+	role: Joi.string().valid(UserRole.EMPLOYEE, UserRole.SCRUM_MASTER).required(),
 }).required();
 
 export const changePasswordSchema = Joi.object<IChangeEmployeePassword>({
 	password: Joi.string().required(),
 	newPassword: Joi.string().invalid(Joi.ref('password')).required(),
 	employeeId: CUSTOM_FIELDS_SCHEMAS.objectId.required(),
-});
+}).required();
+
+export const getAllEmployeeForScrumSchema = Joi.object<IOnlyObjectId>({
+	scrumId: CUSTOM_FIELDS_SCHEMAS.objectId.required(),
+}).required();
